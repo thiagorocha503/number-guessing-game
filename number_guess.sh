03f723fd527484047211c1b366b4bd99ee7c3d14
Thiago Ferreira <thiagorocha503@gmail.com>
2023-06-26 19:50:27 -0300

    feat: Add secret number generator

--- number_guess.sh
+++ number_guess.sh
@@ -19,3 +19,26 @@ else
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 fi
+
+SECRET_NUMBER=$(( ($RANDOM % 1000) + 1 ))
+
+echo -n "Guess the secret number between 1 and 1000: "
+read INPUT_NUMBER
+
+while [ $INPUT_NUMBER != $SECRET_NUMBER ]
+do
+  if [[ ! $INPUT_NUMBER =~ [0-9]+ ]] 
+  then
+    echo -n "That is not an integer, guess again: "
+    read INPUT_NUMBER   
+    continue
+  fi
+  if [[ $INPUT_NUMBER -ge $SECRET_NUMBER ]]
+  then
+    echo -n "It's lower than that, guess again: "    
+  else
+    echo -n "It's higher than that, guess again: "
+  fi
+  read INPUT_NUMBER
+  (( GUESSES++ ))
+done
